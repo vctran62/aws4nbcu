1826c72911aea879fe61c638dcfa63a75536bd5b
Vinh Tran <vctran62@users.noreply.github.com>
2021-02-06 00:03:03 -0500

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,17 +1,12 @@
 data "aws_ami" "latest_ubuntu" {
   most_recent = true
-  owners      = ["330946098119"]
-
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
 }
-
 data "aws_ami" "latest_amazon_windows_2019" {
   most_recent = true
-  owners      = ["amazon"]
-
   filter {
     name   = "name"
     values = ["Windows_Server-2019-English-Full-Base-*"]
