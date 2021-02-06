48208ac55f7ccecafb03d7d298860dc6455fb0c3
Vinh Tran <vctran62@users.noreply.github.com>
2021-02-05 21:54:05 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -9,6 +9,4 @@ variable os                 {}
 
 locals {
     instance_type = var.instance_size == "small" ? "t3.small" : var.instance_size == "medium" ? "t3.medium" : var.instance_size == "large" ? "t3.large" : "t2.micro"
-    ami = var.os == "server2019" ? "ami-06a741c6babd0a9e7" : var.os == "ubuntu" ? data.aws_ami.latest_ubuntu.id : data.aws_ami.latest_ubuntu.id
-}
-“ami-06a741c6babd0a9e7”: var.os == “ubuntu” ? “ami-064d605f100c81fce“
+    ami = var.os == "server2019" ? "ami-06a741c6babd0a9e7" : var.os == "ubuntu" ? "ami-064d605f100c81fce" : "ami-064d605f100c81fce"
