ffedc5456353074ee5a68730d1cab69dfefc4def
Vinh Tran <vctran62@users.noreply.github.com>
2022-04-13 13:25:46 -0400

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "latest_ubuntu" {
   most_recent = true
-  owners      = ["330946098119"]
+  owners      = ["099720109477"]
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
@@ -8,7 +8,7 @@ data "aws_ami" "latest_ubuntu" {
 }
 data "aws_ami" "latest_amazon_windows_2019" {
   most_recent = true
-  owners      = ["330946098119"]
+  owners      = ["801119661308"]
   filter {
     name   = "name"
     values = ["Windows_Server-2019-English-Full-Base-*"]
